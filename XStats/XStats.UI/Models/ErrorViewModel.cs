0f0d4e369df4279d44097e549b33368321820977
Yurii Kleban <yurakleban@gmail.com>
2022-09-19 11:02:45 +0300

    Done

---
+++ XStats/XStats.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XStats.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
